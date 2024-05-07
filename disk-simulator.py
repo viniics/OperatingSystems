fad8f7269fe19d3eb2d18837788d559f163bec8d
Vinicius <viniipvp@gmail.com>
2024-05-07 02:28:58 -0300

    graph generator

--- disk-simulator.py
+++ disk-simulator.py
@@ -1,3 +1,6 @@
+import random
+import matplotlib.pyplot as plt
+import numpy as np
 # Problema:
 #   Escreva um simulador de disco que permita indicar qual a latência de acesso
 #   para uma lista de blocos indicados como dados de entrada.
@@ -29,16 +32,16 @@ capacidade_disco = (setores_por_disco * bytes_por_setor) /1024 #Divisão para ob
 tamanho_bloco = bytes_por_setor
 blocos_por_trilha = setores_por_trilha
 #consideramos um disco cheio onde os blocos foram escritos sequencialmente na trilha, nesse caso: (0,1,2,3...719)
-#
-
 tempo_seek_adjascente = 6 #ms
 tempo_seek_avg = 77 #ms
 tempo_rotacao = 200 #ms
 tempo_transferencia = 22 #ms
 
-#latencia total = tempo de busca + tempo de rotação
+bloco_inicial_disco = 0
 
-def latenciaAcessoBloco(blocoInicial, blocoDesejado):
+#latencia total = tempo de busca + tempo de rotação + tempo transferencia
+
+def latenciaAcessoBloco(blocoInicial, blocoDesejado,printResults):
 
     diferencaDeTrails = trailDifference(blocoInicial, blocoDesejado)
     tempoRotacaoDasTrails = tempoRotacaoSameTrail(blocoInicial, blocoDesejado)
@@ -50,11 +53,11 @@ def latenciaAcessoBloco(blocoInicial, blocoDesejado):
         tempoIndividualBloco = tempoRotacaoDasTrails + tempo_seek_adjascente
     else:
         tempoIndividualBloco = tempoRotacaoDasTrails + tempo_seek_avg
-
-    print(f"O tempo de acesso do bloco {blocoDesejado} saindo do bloco {blocoInicial}"
-          f" foi de {tempoIndividualBloco:.2f} ms")
+    tempoIndividualBloco += tempo_transferencia
+    if printResults:
+        print(f"O tempo de acesso do bloco {blocoDesejado} saindo do bloco {blocoInicial}"
+            f" foi de {tempoIndividualBloco:.2f} ms")
     return tempoIndividualBloco
-
 
 def tempoRotacaoSameTrail(bloco1,bloco2):
     # Calcula o número do setor em que cada bloco está, são 9 setores por trail
@@ -69,26 +72,80 @@ def tempoRotacaoSameTrail(bloco1,bloco2):
         diferenca_setores = setor_bloco2 - setor_bloco1
 
     tempo_medio_rotacao = tempo_rotacao / setores_por_trilha
-    # TODO: print de cada rotacao individual pode vir aqui
+    
     return diferenca_setores * tempo_medio_rotacao
 
 def findBlockTrail(bloco):
-    return bloco//9
+    return bloco//setores_por_trilha
 
 def trailDifference(blocoAtual, blocoDesejado):
     return abs(findBlockTrail(blocoAtual) - findBlockTrail(blocoDesejado))
 
+def sortBySSTF(blocos, blocoInicial):
+    aux = list(blocos)  # Faz uma cópia da lista original
+    lista_ordenada = []
+    while aux:
+        min_latency = float('inf')
+        toBeRemoved = None
+        for i in range(len(aux)):
+            latency = latenciaAcessoBloco(blocoInicial, aux[i], False)
+            if latency < min_latency:
+                min_latency = latency
+                toBeRemoved = i
+        lista_ordenada.append(aux.pop(toBeRemoved))
+        blocoInicial = lista_ordenada[-1]
+    return lista_ordenada
+
 def latenciaAcessoTotal(lista_blocos):
     latencia_total = 0 
-    #Consideramos que o disco começa no bloco 0 em sua inicialização
-    bloco_inicial = 0
+    #Consideramos que o disco começa no bloco 0 na sua inicianiliazição
+    bloco_inicial = bloco_inicial_disco
+    lista_blocos = sortBySSTF(lista_blocos,bloco_inicial)
     for bloco in lista_blocos:
-        latencia_total += latenciaAcessoBloco(bloco_inicial,bloco)
+        latencia_total += latenciaAcessoBloco(bloco_inicial,bloco,True)
         bloco_inicial = bloco
     return latencia_total
 
-blocos = [1,9,1,9,12,20,4,20,4,3,20,0,1,8,1,10,20,10,10,20,25,1]
-tempo = latenciaAcessoTotal(blocos)
-print(f"\nO tempo total latência foi: {tempo:.2f} ms")
+def gerarListaLatencias(lista_blocos):
+    lista_latencias = []
+    bloco_inicial = bloco_inicial_disco
+    lista_blocos = sortBySSTF(lista_blocos,bloco_inicial)
+    for bloco in lista_blocos:
+        lista_latencias.append(latenciaAcessoBloco(bloco_inicial,bloco,False))
+        bloco_inicial = bloco
+    return lista_latencias
 
-# TODO: access blocks by input
+def gerarBlocos(numero_blocos):
+    randomlist = random.choices(range(setores_por_disco), k = numero_blocos)
+    return randomlist
+
+
+blocos = gerarBlocos(int(input("Digite a quantidade de blocos que voce quer gerar: ")))
+list_size = len(blocos)
+
+if any(bloco >= setores_por_disco for bloco in blocos):
+    print(f"Você tentou acessar um bloco que não existe no disco."
+        f"\nMaior bloco possível: {setores_por_disco-1} ")
+    exit()
+
+if(list_size == 0):
+    print("lista vazia")
+    exit()
+
+
+tempo = latenciaAcessoTotal(blocos)
+print(f"\nO tempo total de latência foi: {tempo:.2f} ms")
+
+lista_Lat = gerarListaLatencias(blocos)
+maxLatencyValue = max(lista_Lat)
+
+#Criacao grafico
+plt.scatter(blocos, lista_Lat)
+plt.ylim(0, maxLatencyValue)
+plt.xlim(0, setores_por_disco)
+plt.text(0, maxLatencyValue * 0.9, f'Blocos acessados: {list_size}', color='blue', fontsize=10)
+media_tempo = tempo / list_size
+plt.axhline(y=media_tempo, color='r', linestyle='-')
+plt.text(setores_por_disco - 1, media_tempo, f'Média: {media_tempo:.2f}', color='r', fontsize=10)
+
+plt.show()
